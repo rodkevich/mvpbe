8695d403849c2be13fa2f337b838c96da6175366
rodkevich <rodkevich@gmail.ru>
2023-02-21 16:43:50 +0300

    implement configs interfaces

--- internal/domain/sample/config.go
+++ internal/domain/sample/config.go
@@ -6,12 +6,12 @@ import (
 	"github.com/rodkevich/mvpbe/pkg/database"
 )
 
-var _ setup.DatabaseConfigProvider = (*Config)(nil)
-
-// DatabaseConfig implements setup.DatabaseConfigProvider
-func (c *Config) DatabaseConfig() *database.Config {
-	return &c.Database
-}
+var (
+	_ setup.DatabaseConfigProvider = (*Config)(nil)
+	_ setup.HTTPConfigProvider     = (*Config)(nil)
+	_ setup.CacheConfigProvider    = (*Config)(nil)
+	_ setup.FeaturesConfigProvider = (*Config)(nil)
+)
 
 // Config for application
 type Config struct {
@@ -19,4 +19,24 @@ type Config struct {
 	HTTP     configs.HTTP
 	Cache    configs.Cache
 	Features configs.Features
+}
+
+// DatabaseConfig implements setup.DatabaseConfigProvider
+func (c *Config) DatabaseConfig() *database.Config {
+	return &c.Database
+}
+
+// HTTPConfig implements setup.HTTPConfigProvider
+func (c *Config) HTTPConfig() *configs.HTTP {
+	return &c.HTTP
+}
+
+// CacheConfig implements setup.CacheConfigProvider
+func (c *Config) CacheConfig() *configs.Cache {
+	return &c.Cache
+}
+
+// FeaturesConfig implements setup.FeaturesConfigProvider
+func (c *Config) FeaturesConfig() *configs.Features {
+	return &c.Features
 }
