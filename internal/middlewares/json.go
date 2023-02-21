7d614d3be2a8717560301cd29cd18215dcc4b6c9
rodkevich <rodkevich@gmail.ru>
2023-02-21 16:49:37 +0300

    add comment

--- internal/middlewares/json.go
+++ internal/middlewares/json.go
@@ -2,7 +2,7 @@ package middlewares
 
 import "net/http"
 
-// JSONHeaderContentType ...
+// JSONHeaderContentType add json header for content type
 func JSONHeaderContentType(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
