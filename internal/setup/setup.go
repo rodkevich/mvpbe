10a220a31558f34d8a6305526c81fcdaeecef91f
rodkevich <rodkevich@gmail.ru>
2023-02-21 16:54:46 +0300

    add configs interfaces http, cache, features

--- internal/setup/setup.go
+++ internal/setup/setup.go
@@ -6,15 +6,23 @@ import (
 	"log"
 
 	"github.com/rodkevich/mvpbe/internal/server"
+	"github.com/rodkevich/mvpbe/pkg/configs"
 	"github.com/rodkevich/mvpbe/pkg/database"
 )
 
 // DatabaseConfigProvider ...
-type DatabaseConfigProvider interface {
-	DatabaseConfig() *database.Config
-}
+type DatabaseConfigProvider interface{ DatabaseConfig() *database.Config }
 
-// Setup ..
+// CacheConfigProvider ...
+type CacheConfigProvider interface{ CacheConfig() *configs.Cache }
+
+// HTTPConfigProvider ...
+type HTTPConfigProvider interface{ HTTPConfig() *configs.HTTP }
+
+// FeaturesConfigProvider ...
+type FeaturesConfigProvider interface{ FeaturesConfig() *configs.Features }
+
+// Setup server.Env
 func Setup(ctx context.Context, cfg interface{}) (*server.Env, error) {
 	var serverEnvOpts []server.Option
 
@@ -22,10 +30,10 @@ func Setup(ctx context.Context, cfg interface{}) (*server.Env, error) {
 		log.Println("configuring database")
 
 		_ = provider.DatabaseConfig() // TODO
-		cs := database.ConnectionStringFromEnv()
-		ps := database.PoolSettingsFromEnv()
 
-		db, err := database.New(ctx, cs, ps)
+		dsl := database.ConnectionStringFromEnv()
+		p := database.PoolSettingsFromEnv()
+		db, err := database.New(ctx, dsl, p)
 		if err != nil {
 			return nil, fmt.Errorf("unable to connect to database: %w", err)
 		}
