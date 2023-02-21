a023dcedc0b5447de0d232d66fe6fcdce4d6e6fd
rodkevich <rodkevich@gmail.ru>
2023-02-21 16:53:19 +0300

    use api.v1

--- internal/server/health.go
+++ internal/server/health.go
@@ -1,30 +1,33 @@
 package server
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/rodkevich/mvpbe/pkg/database"
+
+	api "github.com/rodkevich/mvpbe/pkg/api/v1"
 )
 
-// HandleHealth ...
+// HandleHealth checks
+// returns if sever env is ready to process
 func HandleHealth(db *database.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		ctx := r.Context()
 		conn, err := db.Pool.Acquire(ctx)
 		if err != nil {
-			log.Println("failed to acquire connection from pool", "error", err)
+			log.Println("failed to get connection from pool", "error", err)
 		}
 		defer conn.Release()
 
 		if err := conn.Conn().Ping(ctx); err != nil {
-			log.Println("failed to ping database", "error", err)
+			log.Println("failed to ping db", "error", err)
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+
 			return
 		}
 
 		w.WriteHeader(http.StatusOK)
-		fmt.Fprintf(w, `{"status": "ok"}`)
+		api.RenderJSON(w, http.StatusOK, nil)
 	}
 }
