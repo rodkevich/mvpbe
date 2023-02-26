4a41c65acec7c04599fa4926c726f7930e5bcd1f
rodkevich <rodkevich@gmail.ru>
2023-02-26 21:31:13 +0300

    change period settings to time type

--- pkg/database/config.go
+++ pkg/database/config.go
@@ -3,14 +3,25 @@ package database
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/kelseyhightower/envconfig"
 )
 
-// Config ...
+// Config contains dsn and pool settings for postgres
 type Config struct {
-	PoolConfig
-	ConnectionConfig
+	Driver            string        `envconfig:"DB_DRIVER" default:"postgres"`
+	Host              string        `envconfig:"DB_HOST" default:"0.0.0.0"`
+	Port              string        `envconfig:"DB_PORT" default:"5432"`
+	User              string        `envconfig:"DB_USER" default:"postgres"`
+	Password          string        `envconfig:"DB_PASS" default:"postgres"`
+	DBName            string        `envconfig:"DB_NAME" default:"postgres"`
+	SSLMode           string        `envconfig:"DB_SSLMODE" default:"disable"` // mode should be either require or disable
+	ConnQuantityMin   int32         `envconfig:"DB_POOL_MIN_CONNS" default:"10"`
+	ConnQuantityMax   int32         `envconfig:"DB_POOL_MAX_CONNS" default:"50"`
+	ConnTimeLifetime  time.Duration `envconfig:"DB_POOL_MAX_CONN_LIFETIME" default:"5m"`
+	ConnTimeIdleMax   time.Duration `envconfig:"DB_POOL_MAX_CONN_IDLE_TIME" default:"1m"`
+	HealthCheckPeriod time.Duration `envconfig:"DB_POOL_HEALTH_CHECK_PERIOD" default:"1m"`
 }
 
 // DatabaseConfig implements setup.DatabaseConfigProvider
@@ -18,48 +29,18 @@ func (c *Config) DatabaseConfig() *Config {
 	return c
 }
 
-// PoolConfig presents settings for pgx pool
-type PoolConfig struct {
-	ConnQuantityMin   int32 `default:"10"`
-	ConnQuantityMax   int32 `default:"50"`
-	ConnTimeIdleMax   int32 `default:"1"` // minutes
-	ConnTimeLifetime  int32 `default:"3"` // minutes
-	HealthCheckPeriod int32 `default:"3"` // minutes
-}
-
-// ConnectionConfig presents settings for postgres
-type ConnectionConfig struct {
-	Host     string `default:"0.0.0.0"`
-	Port     string `default:"5432"`
-	User     string `default:"postgres"`
-	Password string `default:"postgres"`
-	DBName   string `default:"postgres"`
-	SSLMode  string `default:"disable"` // mode should be either require or disable
-}
-
-// ConnectionStringFromEnv for postgres database
-func ConnectionStringFromEnv() string {
-	s := ConnectionSettingsFromEnv()
+// DsnFromConfig for postgres database
+func DsnFromConfig(c *Config) string {
 	return fmt.Sprintf(
 		"host=%s port=%s user=%s dbname=%s sslmode=%s password=%s",
-		s.Host, s.Port, s.User, s.DBName, s.SSLMode, s.Password,
+		c.Host, c.Port, c.User, c.DBName, c.SSLMode, c.Password,
 	)
 }
 
-// ConnectionSettingsFromEnv for postgres database
-func ConnectionSettingsFromEnv() *ConnectionConfig {
-	var s ConnectionConfig
-	err := envconfig.Process("DB", &s)
-	if err != nil {
-		log.Fatal(err.Error())
-	}
-	return &s
-}
-
-// PoolSettingsFromEnv for postgres database instance
-func PoolSettingsFromEnv() *PoolConfig {
-	var s PoolConfig
-	err := envconfig.Process("DB", &s)
+// DBSettingsFromEnv for postgres database instance
+func DBSettingsFromEnv() *Config {
+	var s Config
+	err := envconfig.Process("", &s)
 	if err != nil {
 		log.Fatal(err.Error())
 	}
