63c5a973d91234251772aa9d693a02dd19c239e6
rodkevich <rodkevich@gmail.ru>
2023-02-26 11:16:37 +0300

    add ping check for opened connection

--- pkg/database/connection.go
+++ pkg/database/connection.go
@@ -11,27 +11,34 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
-// DB ...
+// DB represents database
 type DB struct {
 	Pool *pgxpool.Pool
 }
 
-// New create new connection using pool
-func New(ctx context.Context, connString string, ps *PoolConfig) (*DB, error) {
+// NewPool create new connection using pool
+func NewPool(ctx context.Context, connString string, ps *PoolConfig) (*DB, error) {
 	poolCfg, err := pgxpool.ParseConfig(connString)
 	if err != nil {
 		return nil, fmt.Errorf("pgxpool.ParseConfig: %w", err)
 	}
 
-	poolCfg.MaxConns = ps.ConnQuantityMax
-	poolCfg.MinConns = ps.ConnQuantityMin
-	poolCfg.HealthCheckPeriod = time.Duration(ps.HealthCheckPeriod)
-	poolCfg.MaxConnIdleTime = time.Duration(ps.ConnTimeIdleMax)
-	poolCfg.MaxConnLifetime = time.Duration(ps.ConnTimeLifetime)
+	if ps != nil {
+		poolCfg.MaxConns = ps.ConnQuantityMax
+		poolCfg.MinConns = ps.ConnQuantityMin
+		poolCfg.HealthCheckPeriod = time.Duration(ps.HealthCheckPeriod)
+		poolCfg.MaxConnIdleTime = time.Duration(ps.ConnTimeIdleMax)
+		poolCfg.MaxConnLifetime = time.Duration(ps.ConnTimeLifetime)
+	}
 
 	connPool, err := pgxpool.NewWithConfig(ctx, poolCfg)
 	if err != nil {
 		return nil, fmt.Errorf("pgxpool.NewWithConfig: %w", err)
+	}
+	// assert connection works
+	err = connPool.Ping(ctx)
+	if err != nil {
+		log.Fatal("connPool.Ping: ", err.Error())
 	}
 	// prometheus collector
 	collector := pgxpoolprometheus.NewCollector(connPool, map[string]string{"postgres": "postgres"})
