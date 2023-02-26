009a8707a16342dbfc1f091b23fb2feb0ef3d4a5
rodkevich <rodkevich@gmail.ru>
2023-02-26 11:14:49 +0300

    naming

--- pkg/database/database.go
+++ pkg/database/database.go
@@ -4,31 +4,31 @@ import (
 	"context"
 	"fmt"
 
-	pgxLib "github.com/jackc/pgx/v5"
+	pgx "github.com/jackc/pgx/v5"
 )
 
 // InTx runs within a transaction with the isolation level.
-func (db *DB) InTx(ctx context.Context, isoLevel pgxLib.TxIsoLevel, in func(tx pgxLib.Tx) error) error {
+func (db *DB) InTx(ctx context.Context, isoLevel pgx.TxIsoLevel, in func(tx pgx.Tx) error) error {
 	conn, err := db.Pool.Acquire(ctx)
 	if err != nil {
 		return fmt.Errorf("get connection: %w", err)
 	}
 	defer conn.Release()
 
-	tx, err := conn.BeginTx(ctx, pgxLib.TxOptions{IsoLevel: isoLevel})
+	tx, err := conn.BeginTx(ctx, pgx.TxOptions{IsoLevel: isoLevel})
 	if err != nil {
 		return fmt.Errorf("transaction start: %w", err)
 	}
 
 	if txError := in(tx); txError != nil {
-		if rbError := tx.Rollback(ctx); rbError != nil {
-			return fmt.Errorf("rolling back transaction: %v (original error: %w)", rbError, txError)
+		if dbError := tx.Rollback(ctx); dbError != nil {
+			return fmt.Errorf("transaction rolling back: %v (db error: %w)", dbError, txError)
 		}
 		return txError
 	}
 
 	if err := tx.Commit(ctx); err != nil {
-		return fmt.Errorf("committing transaction: %w", err)
+		return fmt.Errorf("transaction commit: %w", err)
 	}
 	return nil
 }
