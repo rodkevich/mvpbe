15032376240ce48b8140a5504452f6972f5d5803
rodkevich <rodkevich@gmail.ru>
2023-03-09 21:06:41 +0300

    let migrator start from script

--- scripts/local-setup.sh
+++ scripts/local-setup.sh
@@ -3,3 +3,4 @@ cp ./examples/example.env .env
 
 docker-compose up -d --build --remove-orphans postgres
 docker-compose up -d --build --remove-orphans rabbitmq
+docker-compose up -d --build --remove-orphans migrator
