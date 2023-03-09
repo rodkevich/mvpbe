f5634cd78dda76af9764e34cc4c9d97474a0956a
rodkevich <rodkevich@gmail.ru>
2023-03-09 20:56:18 +0300

    add rabbitmq cli

---
+++ cmd/cli/cli.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"log"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func main() {
+	getenv := os.Getenv("AMQP_URI")
+
+	conn, err := amqp.Dial(getenv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+
+	println("connected to:", getenv)
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if len(os.Args) < 2 {
+		log.Printf("Usage: %s [que_name]...", os.Args[0])
+		os.Exit(1)
+	}
+
+	msgs, err := ch.Consume(
+		os.Args[1], // que name
+		"cli",      // consumer
+		true,       // auto ack
+		false,      // exclusive
+		false,      // non local
+		false,      // no wait
+		nil,        // args
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var forever chan struct{}
+
+	go func() {
+		for d := range msgs {
+			log.Printf("[+] %s", d.Body)
+		}
+	}()
+
+	<-forever
+}
